0c2b6c1607bbbf8e0b5922d20e2c42c41b58e975
Lucas <lucasjaskowiak@yahoo.fr>
2025-01-30 10:14:20 +0100

    Ajout de nouvelles potions dans l'inventaire

--- exercices.js
+++ exercices.js
@@ -22,3 +22,33 @@ function creerPotion(idPotion, prix = 10, stock = 1) {
 }
 console.log(creerPotion("Potion de soin"));
 console.log(creerPotion("Potion de magie", 8, 5));
+
+// Ajout de nouvelles potions dans l'inventaire
+let inventaire = [["potion de magie", 4, 50]];
+
+function ajouterPotion(inventaire, nomPotion) {
+  if (inventaire.length === 0) {
+    const prix = parseInt(prompt("Quel est le prix unitaire de la potion?"));
+    const qté = parseInt(prompt(`Quel est la quantité de ${nomPotion} à ajouter au stock?`));
+    const potion = [nomPotion, prix, qté];
+    inventaire.push(potion);
+  } else {
+    for (let i = 0; i < inventaire.length; i++) {
+      if (inventaire[i][0] != nomPotion) {
+        const prix = parseInt(prompt("Quel est le prix unitaire de la potion?"));
+        const qté = parseInt(prompt(`Quel est la quantité de ${nomPotion} à ajouter au stock?`));
+        const potion = [nomPotion, prix, qté];
+        inventaire.push(potion);
+        break;
+      } else if (inventaire[i][0] === nomPotion) {
+        console.log("existe !");
+        inventaire[i][1] = parseInt(prompt("Quel est le nouveau prix de la potion?"));
+        inventaire[i][2] += parseInt(prompt("Quel est la quantité de potion à ajouter?"));
+      }
+    }
+  }
+  inventaire = inventaire.sort((a, b) => b[1] - a[1]);
+}
+
+ajouterPotion(inventaire, "potion de soin");
+console.log(inventaire);
