bfb37fc916a5a4f3b2cac345f6d93ff0f1010a4b
Lucas <lucasjaskowiak@yahoo.fr>
2025-01-30 08:57:36 +0100

    Fabrication de potion

--- exercices.js
+++ exercices.js
@@ -6,8 +6,19 @@ function salutations(nomSorcier) {
 salutations("Lucas");
 
 // Quel est le tarif d'une potion ?
-function tarifPotion(qté, prixPotion) {
+function tarifPotion(qté = 1, prixPotion) {
   return prixPotion * qté;
 }
 
 console.log(tarifPotion(3, 10));
+
+// Fabrication de potion
+function creerPotion(idPotion, prix = 10, stock = 1) {
+  return {
+    id: idPotion,
+    prix: prix,
+    stock: stock,
+  };
+}
+console.log(creerPotion("Potion de soin"));
+console.log(creerPotion("Potion de magie", 8, 5));
